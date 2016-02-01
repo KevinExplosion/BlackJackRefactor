7653c52811abda088e31c956cc5d15526c63bdd5
isabelle george and kevin mattison <mattisonii@yahoo.com>
2016-02-01 14:57:14 -0800

    Merge game functionality with front page styling.

--- js/master.js
+++ js/master.js
@@ -12,7 +12,7 @@ function Player(name, score, chips) {
 
 function randomCardSet(){
   var randomSet = Math.round(Math.random() * (4 - 1) + 1);
-  
+
   if(randomSet === 1){
     return "Hearts";
   } else if(randomSet === 2){
@@ -35,13 +35,13 @@ function randomCard() {
   } else if(randomNum === 5) {
     return 5 + randomCardSet();
   } else if(randomNum === 6) {
-    return 6 + randomCardSet();  
+    return 6 + randomCardSet();
   } else if(randomNum === 7) {
     return 7 + randomCardSet();
   } else if(randomNum === 8) {
     return 8 + randomCardSet();
   } else if(randomNum === 9) {
-    return 9 + randomCardSet();  
+    return 9 + randomCardSet();
   } else if(randomNum === 10) {
     return 10 + randomCardSet();
   } else{
@@ -50,32 +50,23 @@ function randomCard() {
 }
 
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
 
 /*=====================JQUERY====================*/
 
->>>>>>> td-ac
+
 $(document).ready(function(){
   $("#start").click(function(){
     $(".introduction").hide();
     $(".game").show("slide");
     var playerOne = $("#player1name").val();
     var newPlayer = new Player(playerOne, 0, 500);
-<<<<<<< HEAD
 
-=======
+
+
     $("#playerNameSpan").text(newPlayer.name);
     $("#remainingChipsSpan").text(newPlayer.chips);
     $("#randomCardBtn").click(function(){
       $("#randomCard").text(randomCard());
     });
-    
->>>>>>> td-ac
   });
 });
