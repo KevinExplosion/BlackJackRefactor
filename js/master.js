94cf07cae445277e61459d6e66a549ce17d3201d
isabelle george and kevin mattison <10izzygeorge@gmail.com>
2016-02-01 11:00:08 -0800

    refactor prototype for total score

--- js/master.js
+++ js/master.js
@@ -4,9 +4,8 @@ function Player(name, score) {
 }
 
 Player.prototype.totalScore = function() {
-  var playerScore = 0
-  while (playerScore < 21){
-    this.score = playerScore += Math.round(Math.random() * (11 - 2) + 2);
+  while (this.score < 21){
+    this.score = this.score += Math.round(Math.random() * (11 - 2) + 2);
   }
   return this.score;
 }
