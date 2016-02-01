8c0e8c93ea9e854c9b37c80f0d23970fca5b9b77
isabelle george and kevin mattison <mattisonii@yahoo.com>
2016-02-01 11:34:04 -0800

    Add text input for player1 & player2 name & age.

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,16 @@
 describe('Player', function() {
   it('defines the name and score for each player', function() {
-    var testPlayer = new Player("Test Pilot", 0);
+    var testPlayer = new Player("Test Pilot", 0, 500);
     expect(testPlayer.name).to.equal("Test Pilot");
     expect(testPlayer.score).to.equal(0);
+    expect(testPlayer.chips).to.equal(500);
   });
   it("concatenates player's randomly generated numbers for a total score", function() {
-    var testPlayer = new Player("Test Pilot", 22);
+    var testPlayer = new Player("Test Pilot", 21, 500);
     expect(testPlayer.totalScore()).to.be.a('number');
+  });
+  it("takes away chips bet from bank", function() {
+    var testPlayer = new Player("Test Pilot", 21, 300);
+    expect(testPlayer.totalScore()).to.equal(200);
   });
 });
